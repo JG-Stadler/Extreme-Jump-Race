ba3cd23834b959cd7d62ad00ada047453c4c2050
JG-Stadler <joaogbstadler@gmail.com>
2023-11-10 19:47:13 -0300

    sistema de verificação de colisão criado

--- js/game.js
+++ js/game.js
@@ -9,11 +9,38 @@ function startGame() {
   city.classList.add("cityActive");
   car.classList.add("carActive");
   clouds.classList.add("cloudsActive");
+
+  ColisionVerification();
 }
 
 jumpButton.addEventListener("click", () => {
-    activatedPlayer.classList.add("playerJump");
-  setTimeout(()=>{
-  activatedPlayer.classList.remove("playerJump");
-},1500);
+  activatedPlayer.classList.add("playerJump");
+  setTimeout(() => {
+    activatedPlayer.classList.remove("playerJump");
+  }, 1500);
 });
+
+// Colision Verification
+
+const gameWindow = document.getElementById("game-window");
+var LimitCarDistance = gameWindow.offsetHeight * 0.17;
+const minPlayerBottom = 100;
+const car = document.getElementById("car");
+
+function ColisionVerification(){
+  setInterval(() =>{
+  const carDistance = car.offsetLeft;
+  const playerBottom = window.getComputedStyle(player).bottom.replace("px", " ");
+
+  if(playerBottom < minPlayerBottom && carDistance <= LimitCarDistance && carDistance > -200){
+      console.log("perdeu")
+      GameOver();
+  }
+},5);
+}
+
+// Stop Game
+
+// function GameOver(){
+
+// }
