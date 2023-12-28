74be1e6d204d0c2b43420d21912bbb17cc95f351
João Stadler <joaogbstadler@gmail.com>
2023-12-28 14:08:10 -0300

    Variaveis de medição da distancia do carro declaradas

--- js/game.js
+++ js/game.js
@@ -8,3 +8,17 @@ JumpButton.addEventListener("click",()=>{
     player.src = "images/player-runing.gif";
     },1000);
 });
+
+// Colision verification / Verificação de colisão
+const gamewindow = document.getElementById("game-window");
+
+let limit_car_distance = player.offsetLeft + player.clientWidth;
+let min_player_height = car.clientHeight;
+let car_out_player_line = gamewindow.clientWidth - player.offsetLeft;
+
+// setInterval(() => {
+//     if(car.right >= limit_car_distance
+//         && car.right <car_out_player_line){
+//             console.log("perdeu")
+//         }
+// }, 50);
