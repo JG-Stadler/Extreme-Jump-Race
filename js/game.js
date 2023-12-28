4d304e18692592db5d17d194dcee8d297aa57ece
João Stadler <joaogbstadler@gmail.com>
2023-12-28 14:21:30 -0300

    verificação de colisão configurada

--- js/game.js
+++ js/game.js
@@ -15,10 +15,20 @@ const gamewindow = document.getElementById("game-window");
 let limit_car_distance = player.offsetLeft + player.clientWidth;
 let min_player_height = car.clientHeight;
 let car_out_player_line = gamewindow.clientWidth - player.offsetLeft;
+let player_jump_height;
+let car_distance;
 
-// setInterval(() => {
-//     if(car.right >= limit_car_distance
-//         && car.right <car_out_player_line){
-//             console.log("perdeu")
-//         }
-// }, 50);
+    const fiviPCCarHeight = car.clientHeight*0.5;
+    const car_height = car.clientHeight - fiviPCCarHeight;
+
+
+setInterval(() => {
+    car_distance = +window.getComputedStyle(car).right.replace("px","");
+    player_jump_height = +window.getComputedStyle(player).bottom.replace("px","");
+
+    if(car_distance >= limit_car_distance
+        && car_distance <car_out_player_line
+        && player_jump_height < car_height){
+            console.log("perdeu")
+        }
+}, 50);
